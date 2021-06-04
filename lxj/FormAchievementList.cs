777975e1ebe50c2f072b6ad9f8618a9c52d67599
lixijun <2511367264@qq.com>
2021-06-04 12:23:17 +0800

    add
    lxj

---
+++ lxj/FormAchievementList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dailyrecord
+{
+    public partial class FormAchievementList : Form
+    {
+        public FormAchievementList()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //重新设置日期时间控件的文本
+            dateTimePicker1.ResetText();
+        }
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+        //事件统计结果柱状图
+        public void createImage1(int AccNum, int UnaccNum)
+        {
+        }
+
+        //番茄钟统计饼状图
+        public void createImage(int AccNum, int UnaccNum)
+        {
+        }
+    }
+}
---
+++ lxj/TCtime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dailyrecord
+{
+    class TCtime
+    {
+        // 根据开始时间和结束时间获得特定时间段的工作时间
+
+        //统计专注工作时间
+        public static double GetTotalWTime(DateTime startdate, DateTime enddate, string groupBy)
+        {
+            
+            
+        }
+        //番茄钟间隔时间
+        public static double GetTotalRTime(DateTime startdate, DateTime enddate, string groupBy)
+        {
+
+
+        }
+    }
+}
