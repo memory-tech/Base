f9f5c72f7e83e24e0bac7c9cfcc757a47c5eb494
xvyang <62140017+serein-dream@users.noreply.github.com>
2021-06-09 22:54:36 +0800

    更新

--- ScheduleManagement/ScheduleManagement/UserControl4.cs
+++ ScheduleManagement/ScheduleManagement/UserControl4.cs
@@ -5,15 +5,39 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using ScheduleManagement.clawer;
 
 namespace ScheduleManagement
 {
     public partial class UserControl4 : UserControl
     {
+        BindingSource resultBindingSource = new BindingSource();
+        clawerbasketball claw = new clawerbasketball();
         public UserControl4()
         {
             InitializeComponent();
+            Result.DataSource = resultBindingSource;
+            claw.NewsDownloaded += Clawer_NewsDownloaded;
+            
+        }
+        private Graphics graphics;
+
+
+        private void Clawer_NewsDownloaded(clawerbasketball crawler, string day, string time ,string player1,string player2,string turn,string link)
+        {
+            var newpage = new { Index = resultBindingSource.Count + 1, Day = day, Time = time,P1 = player1,P2=player2,T=turn,detail = link };
+            Action action = () => { resultBindingSource.Add(newpage); };
+            if (this.InvokeRequired)
+            {
+                this.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -22,6 +46,17 @@ namespace ScheduleManagement
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            resultBindingSource.Clear();
+            claw.Excute();
+           // Task task = Task.Run(() => claw.Excute());
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
--- ScheduleManagement/ScheduleManagement/clawer/clawerbasketball.cs
+++ ScheduleManagement/ScheduleManagement/clawer/clawerbasketball.cs
@@ -16,7 +16,8 @@ namespace ScheduleManagement.clawer
 {
     class clawerbasketball
     {
-        
+
+        public event Action<clawerbasketball, string, string, string, string, string, string> NewsDownloaded;
         //要爬取的网址
         public string requestUrl = "https://m.hupu.com/soccer/csl/match";
         
@@ -45,81 +46,91 @@ namespace ScheduleManagement.clawer
         }
         public bool JieXiHtml(string html)
         {
-          //  using (RISDBContext dbContext = new RISDBContext())
-          //  {
-                var doc = this.CreateHtmlDocument(html);
-                //下面的意思是：通过属性class的值，来定位节点信息
-                HtmlNodeCollection htmlNodes = doc.DocumentNode.SelectNodes("//section[@class='match-section match-today']");
+            //  using (RISDBContext dbContext = new RISDBContext())
+            //  {
+            var doc = this.CreateHtmlDocument(html);
+            //下面的意思是：通过属性class的值，来定位节点信息
+            HtmlNodeCollection htmlNodes = doc.DocumentNode.SelectNodes("//section[@class='match-section match-today']");
 
-                if (htmlNodes.Count == 0)
-                    return false;//没取到，说明没赛程安排
-                var htmlNodeList = htmlNodes.ToList();
+            if (htmlNodes.Count == 0)
+                return false;//没取到，说明没赛程安排
+            var htmlNodeList = htmlNodes.ToList();
 
-                for (var i = 0; i < htmlNodeList.Count; i++)
-                {
-                    var htmlNode = htmlNodeList[i];
-                    //取页面section中的内容
-                    //用temp而不是htmlNode再进行相关的赋值即可
-                    var temp = HtmlNode.CreateNode(htmlNode.OuterHtml);
-                    var news = new News();
-                    //取日期，几号
-                    var DayHtmlNode = temp.SelectSingleNode("//div[@class='time-label']/span");
-                    news.Day = DayHtmlNode == null ? "" : DayHtmlNode.InnerText;
-                    //取详情页面url    href?
-                    var UrlHtmlNode = temp.SelectSingleNode("//div[@class='time-label']/href");
-                    news.Link = UrlHtmlNode == null ? "" : UrlHtmlNode.InnerText;
-                    //具体几点
-                    var TimeHtmlNode = temp.SelectSingleNode("//div[@class='match-state']");
-                    news.Time = TimeHtmlNode == null ? "" : TimeHtmlNode.InnerText;
-                    //球队1
-                    var Player1HtmlNode = temp.SelectSingleNode("//div[@class='team-name']/div[0]/h4");
-                    news.Player1 = Player1HtmlNode == null ? "" : Player1HtmlNode.InnerText;
-                    //球队2
-                    var Player2HtmlNode = temp.SelectSingleNode("//div[@class='team-name']/div[1]/h4");
-                    news.Player2 = Player2HtmlNode == null ? "" : Player2HtmlNode.InnerText;
-                    //场次
-                    var TurnHtmlNode = temp.SelectSingleNode("//div[@class='match-played']");
-                    news.Turn = TurnHtmlNode == null ? "" : TurnHtmlNode.InnerText;
-           
-                    //对比赛详情页的查询统计,首场6.21
-                    /*
-                    if (!string.IsNullOrEmpty(news.Link) && news.Link.Contains("http://"))
-                    {
-                        var contentHtml = this.LoadUrl(news.Link, Encoding.Default);
-                        var contentDoc = this.CreateHtmlDocument(contentHtml);
-                        HtmlNode sourceHtmlNode = contentDoc.DocumentNode.SelectSingleNode("//div[@id='cont_1_1_2']/div[@class='left-time']/div[@class='left-t']");
-                        news.Source = sourceHtmlNode == null ? "" : sourceHtmlNode.InnerText;
-                        HtmlNode contentHtmlNode = contentDoc.DocumentNode.SelectSingleNode("//div[@id='cont_1_1_2']/div[@class='left_zw']");
-                        news.Content = contentHtmlNode == null ? "" : contentHtmlNode.InnerHtml;
-                        HtmlNode authorHtmlNode = contentDoc.DocumentNode.SelectSingleNode("//div[@id='cont_1_1_2']/div[@class='left_name']/div[@class='left_name']");
-                        news.Author = authorHtmlNode == null ? "" : authorHtmlNode.InnerText;
-                    }
-                    */
-                //保存到数据库    dbContext.News.Add(news);
-            //    }
-                try
-                {
-                 //保存到数据库   dbContext.SaveChanges();
-                }
-                catch (Exception ex)
-                {
+            for (var i = 0; i < htmlNodeList.Count; i++)
+            {
+                var htmlNode = htmlNodeList[i];
+                //取页面section中的内容
+                //用temp而不是htmlNode再进行相关的赋值即可
+                var temp = HtmlNode.CreateNode(htmlNode.OuterHtml);
+                var news = new News();
+                //取日期，几号
+                var DayHtmlNode = temp.SelectSingleNode("//div[@class='time-label']/span");
+                news.Day = DayHtmlNode == null ? "" : DayHtmlNode.InnerText;
+                //取详情页面url    href?
+                var UrlHtmlNode = temp.SelectSingleNode("//div[@class='time-label']/href");
+                news.Link = UrlHtmlNode == null ? "" : UrlHtmlNode.InnerText;
+                //具体几点
+                var TimeHtmlNode = temp.SelectSingleNode("//div[@class='match-state']");
+                news.Time = TimeHtmlNode == null ? "" : TimeHtmlNode.InnerText;
+                //球队1
+                var Player1HtmlNode = temp.SelectSingleNode("//div[@class='team-name']/div[1]/h4");
+                news.Player1 = Player1HtmlNode == null ? "" : Player1HtmlNode.InnerText;
+                //球队2
+                var Player2HtmlNode = temp.SelectSingleNode("//div[@class='team-name']/div[2]/h4");
+                news.Player2 = Player2HtmlNode == null ? "" : Player2HtmlNode.InnerText;
+                //场次
+                var TurnHtmlNode = temp.SelectSingleNode("//div[@class='match-played']");
+                news.Turn = TurnHtmlNode == null ? "" : TurnHtmlNode.InnerText;
+                NewsDownloaded(this, news.Day, news.Time, news.Player1, news.Player2, news.Turn, news.Link);
+            }
+            return true;
+        }
 
-                }
+            //对比赛详情页的查询统计,首场6.21
+            /*
+            if (!string.IsNullOrEmpty(news.Link) && news.Link.Contains("http://"))
+            {
+                var contentHtml = this.LoadUrl(news.Link, Encoding.Default);
+                var contentDoc = this.CreateHtmlDocument(contentHtml);
+                HtmlNode sourceHtmlNode = contentDoc.DocumentNode.SelectSingleNode("//div[@id='cont_1_1_2']/div[@class='left-time']/div[@class='left-t']");
+                news.Source = sourceHtmlNode == null ? "" : sourceHtmlNode.InnerText;
+                HtmlNode contentHtmlNode = contentDoc.DocumentNode.SelectSingleNode("//div[@id='cont_1_1_2']/div[@class='left_zw']");
+                news.Content = contentHtmlNode == null ? "" : contentHtmlNode.InnerHtml;
+                HtmlNode authorHtmlNode = contentDoc.DocumentNode.SelectSingleNode("//div[@id='cont_1_1_2']/div[@class='left_name']/div[@class='left_name']");
+                news.Author = authorHtmlNode == null ? "" : authorHtmlNode.InnerText;
+            }
+            */
+            //保存到数据库    dbContext.News.Add(news);
+            //     }
+            /*
+            try
+            {
+             //保存到数据库   dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
 
-                return true;//继续爬取
             }
 
-            return false;
+            return true;//继续爬取
         }
-       
 
-        private void startBtn_Click(object sender, EventArgs e)
+        return false;
+            
+               
+        }
+       */
+
+        public void Excute()//(object sender, EventArgs e)
         {
-            var isTrue = true;
+           // var isTrue = true;
+            this.Start(requestUrl);
+            /*
             while (isTrue)
             {
                 isTrue = this.Start(requestUrl);
             }
+            */
         }
 
         public bool Start(string url)
