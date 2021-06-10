79e964716a0786c614904af9880aea4d3daa135d
memory-tech <2306821735@qq.com>
2021-06-10 23:24:04 +0800

    修改番茄钟

--- ScheduleManagement/ScheduleManagement/Tomato.Designer.cs
+++ ScheduleManagement/ScheduleManagement/Tomato.Designer.cs
@@ -29,44 +29,17 @@ namespace ScheduleManagement
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.btnBegin = new System.Windows.Forms.Button();
             this.btnEnd = new System.Windows.Forms.Button();
             this.TimeSet = new System.Windows.Forms.Label();
-            this.BreakSet = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.cmbRemindStyle = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.label5 = new System.Windows.Forms.Label();
-            this.cmbRemindStyle = new System.Windows.Forms.ComboBox();
             this.panel1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("宋体", 12F);
-            this.label1.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.label1.Location = new System.Drawing.Point(91, 17);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 27);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "番茄时长:";
-            // 
-            // label2
-            // 
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("宋体", 12F);
-            this.label2.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.label2.Location = new System.Drawing.Point(260, 17);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(103, 27);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "休息时长:";
             // 
             // btnBegin
             // 
@@ -99,64 +72,23 @@ namespace ScheduleManagement
             this.TimeSet.AutoSize = true;
             this.TimeSet.Font = new System.Drawing.Font("隶书", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.TimeSet.ForeColor = System.Drawing.Color.LightSeaGreen;
-            this.TimeSet.Location = new System.Drawing.Point(81, 75);
+            this.TimeSet.Location = new System.Drawing.Point(179, 52);
             this.TimeSet.Name = "TimeSet";
             this.TimeSet.Size = new System.Drawing.Size(122, 40);
             this.TimeSet.TabIndex = 10;
             this.TimeSet.Text = "15:00";
-            // 
-            // BreakSet
-            // 
-            this.BreakSet.AutoSize = true;
-            this.BreakSet.Font = new System.Drawing.Font("隶书", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.BreakSet.ForeColor = System.Drawing.Color.LightSeaGreen;
-            this.BreakSet.Location = new System.Drawing.Point(257, 75);
-            this.BreakSet.Name = "BreakSet";
-            this.BreakSet.Size = new System.Drawing.Size(122, 40);
-            this.BreakSet.TabIndex = 11;
-            this.BreakSet.Text = "05:00";
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.panel1.Controls.Add(this.cmbRemindStyle);
             this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.BreakSet);
             this.panel1.Controls.Add(this.TimeSet);
-            this.panel1.Controls.Add(this.label2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 28);
+            this.panel1.Location = new System.Drawing.Point(0, 30);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(485, 192);
             this.panel1.TabIndex = 12;
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.设置ToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(485, 28);
-            this.menuStrip1.TabIndex = 13;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // 设置ToolStripMenuItem
-            // 
-            this.设置ToolStripMenuItem.Name = "设置ToolStripMenuItem";
-            this.设置ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
-            this.设置ToolStripMenuItem.Text = "设置";
-            this.设置ToolStripMenuItem.Click += new System.EventHandler(this.设置ToolStripMenuItem_Click);
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(99, 150);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(67, 15);
-            this.label5.TabIndex = 15;
-            this.label5.Text = "提醒方式";
             // 
             // cmbRemindStyle
             // 
@@ -170,6 +102,33 @@ namespace ScheduleManagement
             this.cmbRemindStyle.Name = "cmbRemindStyle";
             this.cmbRemindStyle.Size = new System.Drawing.Size(121, 23);
             this.cmbRemindStyle.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(99, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 15);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "提醒方式";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.设置ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(485, 30);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 设置ToolStripMenuItem
+            // 
+            this.设置ToolStripMenuItem.Name = "设置ToolStripMenuItem";
+            this.设置ToolStripMenuItem.Size = new System.Drawing.Size(53, 26);
+            this.设置ToolStripMenuItem.Text = "设置";
+            this.设置ToolStripMenuItem.Click += new System.EventHandler(this.设置ToolStripMenuItem_Click);
             // 
             // Tomato
             // 
@@ -193,13 +152,9 @@ namespace ScheduleManagement
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnBegin;
         private System.Windows.Forms.Button btnEnd;
         private System.Windows.Forms.Label TimeSet;
-        private System.Windows.Forms.Label BreakSet;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem 设置ToolStripMenuItem;
