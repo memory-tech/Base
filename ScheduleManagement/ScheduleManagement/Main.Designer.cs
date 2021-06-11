54dd2a51be4960cfc5823b411af55a16085e7857
lixijun <2511367264@qq.com>
2021-06-11 10:46:41 +0800

    1
    1

--- ScheduleManagement/ScheduleManagement/Main.Designer.cs
+++ ScheduleManagement/ScheduleManagement/Main.Designer.cs
@@ -137,7 +137,7 @@
             this.退出ToolStripMenuItem1});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1148, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(1148, 30);
             this.menuStrip1.TabIndex = 3;
             this.menuStrip1.Text = "menuStrip1";
             this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
