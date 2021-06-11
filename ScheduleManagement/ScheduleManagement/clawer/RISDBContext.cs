587d890dd2ae8fc794a5e59115bef5245f85ea6e
lixj111 <79953753+lixj111@users.noreply.github.com>
2021-06-11 16:40:06 +0800

    Add files via upload

---
+++ ScheduleManagement/ScheduleManagement/clawer/RISDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleManagement.clawer
+{
+    class RISDBContext
+    {
+        public DbSet<News> News { get; set; }
+        void SaveChanges() { }
+    }
+}
